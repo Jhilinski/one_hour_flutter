fbd066464bdca28b23e51efd1610f01a46683dfa
Joe Hilinski <jhilinski@yahoo.com>
2022-07-13 17:04:36 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/one_hour_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.one_hour_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import './learn_flutter_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  get builder => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return const LearnFlutterPage();
+              },
+            ),
+          );
+        },
+        child: const Text('Learn Flutter'),
+      ),
+    );
+  }
+}
